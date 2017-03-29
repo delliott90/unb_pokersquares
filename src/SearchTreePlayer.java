9c661e348eb0cd440a2aea46efbb89ad0618fad8
Danny Elliott <danny.elliott@gmail.com>
2017-03-28 23:31:07 -0300

    Grid score multiplier likelihood of flush, straights, and royal flush

--- src/SearchTreePlayer.java
+++ src/SearchTreePlayer.java
@@ -1,6 +1,6 @@
 /**
  * SearchTreePlayer - a simple example implementation of the player interface for PokerSquares that 
- * ADD MORE STUFF HERE.
+ * evaluates the search tree to a level of 2
  * Ordering of suits in the play grid are as follows:
  * Column 0: Clubs, Column 1: Diamonds, Column 2: Hearts, Column 3: Spades, Column 4: Overflow
  * Author: Danny Elliott, based on code provided by Todd W. Neller and Michael Fleming
@@ -14,7 +14,8 @@ public class SearchTreePlayer implements PokerSquaresPlayer {
     private PokerSquaresPointSystem system; // point system
     private final int DEPTH_LIMIT = 2;
     private Card[] cardsInDeck = new Card[NUM_CARDS];
-    private int[] bestScoreAndPosition = new int[3]; // row, col, score
+    private int[] bestPosition = new int[2]; // row, col
+    private double bestScore = 0.0;
     private int cardsOnGrid = 0;
 	
 	/* (non-Javadoc)
@@ -36,22 +37,15 @@ public class SearchTreePlayer implements PokerSquaresPlayer {
 	 */
 	@Override
 	public int[] getPlay(Card card, long millisRemaining) {
-        int cardrow = 0;
-        int cardcol = 0;
-
-        int cardrank = card.getRank();
-        int cardsuit = card.getSuit(); // 0-3
 
         initializeBestScorePosition(grid);
         cardsOnGrid++;
 
         depthSearch(card);
 
-        placeCard(card, bestScoreAndPosition[0], bestScoreAndPosition[1], grid); // Place card on main grid
-        int[] playPosition = {bestScoreAndPosition[0], bestScoreAndPosition[1]};
-
+        placeCard(card, bestPosition[0], bestPosition[1], grid); // Place card on main grid
+        int[] playPosition = {bestPosition[0], bestPosition[1]};
         return playPosition;
-
     }
 
     private Card[][] copyGrid(Card[][] inputGrid){
@@ -72,18 +66,166 @@ public class SearchTreePlayer implements PokerSquaresPlayer {
 
     private void initializeBestScorePosition(Card[][] grid){
         int[] emptySpot = findFirstEmptySpot(grid, false);
-        bestScoreAndPosition[0] = emptySpot[0];
-        bestScoreAndPosition[1] = emptySpot[1];
-        bestScoreAndPosition[2] = 0;
+        bestPosition[0] = emptySpot[0];
+        bestPosition[1] = emptySpot[1];
+        bestScore = 0.0;
     }
 
     private void scoreGrid(int row, int col, Card[][] grid){
-        int stateScore = system.getScore(grid);
-        if (bestScoreAndPosition[2] < stateScore){
-            bestScoreAndPosition[0] = row;
-            bestScoreAndPosition[1] = col;
-            bestScoreAndPosition[2] = stateScore;
+        double stateScore = (double)system.getScore(grid);
+        double scoreMultiplier = 1.0;
+
+        // Modify stateScore based on probability of certain hands
+
+        for(int r=0; r<SIZE; r++){
+            Card[] rowHand = grid[r];
+            scoreMultiplier += evaluateHand(rowHand);
         }
+
+        for(int c=0; c < SIZE; c++){
+            Card[] columnHand = new Card[SIZE];
+            for(int r=0; r<SIZE; r++){
+                columnHand[r] = grid[r][c];
+            }
+            scoreMultiplier += evaluateHand(columnHand);
+        }
+
+        stateScore *= scoreMultiplier;
+
+        if (bestScore < stateScore){
+            bestPosition[0] = row;
+            bestPosition[1] = col;
+            bestScore = stateScore;
+        }
+    }
+
+    private double evaluateHand(Card[] hand){
+        double resultScore = 0.0;
+        double flushScore = 0.0;
+        double straightScore = 0.0;
+        double royalScore = 0.0;
+
+        flushScore = (double)flushCheck(hand);
+        if(flushScore > 0){ resultScore += flushScore; }
+        straightScore = (double)straightCheck(hand);
+        if(straightScore > 0){ resultScore += straightScore; }
+        if(straightScore > 2 && flushScore > 2){ resultScore += 5; }
+        if(flushScore > 2){
+            royalScore = (double)royalCheck(hand);
+            if(royalScore > 0){ resultScore += royalScore * 2; }
+        }
+        return resultScore;
+    }
+
+    private int flushCheck(Card[] hand){
+        int returnCount = 0;
+        int suit = -1;
+        for(int i=0; i<hand.length; i++){
+            if(hand[i] != null){
+                int currentCardSuit = hand[i].getSuit();
+                if(suit == -1){
+                    suit = currentCardSuit;
+                    returnCount ++; // set to 1
+                }
+                else if (suit == currentCardSuit){
+                    returnCount ++;
+                }
+                else{
+                    return -1; // chance of flush ruined
+                }
+            }
+        }
+        return returnCount; // returns 5 if flush
+    }
+
+    private int straightCheck(Card[] hand){
+        int returnCount = 0;
+        int rank = -1;
+        int nilCount = 0;
+        for(int i=0; i<hand.length; i++){
+            if(hand[i] == null){
+                if(rank == -1){
+                    continue; // only null cards so far
+                }
+                else{
+                    nilCount ++; // track how many nulls between real cards
+                }
+            }
+            else{
+                int currentCardRank = hand[i].getRank();
+                if( currentCardRank < i || currentCardRank > (Card.NUM_RANKS-(hand.length-(i+1))) ){
+                    return -1; // rank out of bounds
+                }
+                if(rank == -1){ // first real card found
+                    rank = currentCardRank;
+                    returnCount ++; // set to 1
+                }
+                else if(currentCardRank == (rank + 1 + nilCount)){
+                    nilCount = 0; // reset nil counter since we found a real card
+                    rank = currentCardRank;
+                    returnCount ++;
+                }
+                else{
+                    return -1;
+                }
+            }
+        }
+        return returnCount; // returns 5 if straight
+    }
+
+    // Check for flush first
+    private int royalCheck(Card[] hand){
+        int returnCount = 1;
+        if(hand[0] != null){
+            if(hand[0].getRank() == 9){
+                returnCount ++;
+            }
+            else{
+                return -1;
+            }
+        }
+        if(hand[1] != null){
+            if(hand[1].getRank() == 10){
+                returnCount ++;
+            }
+            else{
+                return -1;
+            }
+        }
+        if(hand[2] != null){
+            if(hand[2].getRank() == 11){
+                returnCount ++;
+            }
+            else{
+                return -1;
+            }
+        }
+        if(hand[3] != null){
+            if(hand[3].getRank() == 12){
+                returnCount ++;
+            }
+            else{
+                return -1;
+            }
+        }
+        if(hand[4] != null){
+            if(hand[4].getRank() == 0){
+                returnCount ++;
+            }
+            else{
+                return -1;
+            }
+        }
+        return returnCount;
+    }
+
+    private boolean fullHand(Card[] hand){
+        for(int i=0; i<hand.length; i++){
+            if(hand[i] == null){
+                return false;
+            }
+        }
+        return true;
     }
 
     private int[] findFirstEmptySpot(Card[][] grid, boolean random){
@@ -138,7 +280,6 @@ public class SearchTreePlayer implements PokerSquaresPlayer {
         int[] rootPosition = new int[2];
         int[] bestSpotFound = findFirstEmptySpot(tempGrid, false);
 
-
         if(DEPTH_LIMIT==1){
             rootPosition = bestSpotFound;
             placeAndScore(card, tempGrid, 1, rootPosition, true);
@@ -153,14 +294,10 @@ public class SearchTreePlayer implements PokerSquaresPlayer {
                         rootPosition[1] = col;
 
                         placeAndScore(card, tempGrid, 1, rootPosition, true);
-
                     }
                 }
             }
-
         }
-
-
     }
 
 
@@ -175,9 +312,10 @@ public class SearchTreePlayer implements PokerSquaresPlayer {
                     for (int col = 0; col < SIZE; col++) {
                         if (grid[row][col] == null && card != null) {
                             placeCard(card, row, col, grid); // MAX NODE
-//                            // This effects performance so don't use this for real. You'll need to lower the number of chance cards or player will run out of time
-//                            system.printGrid(grid);
-//                            System.out.println("");
+                            // This effects performance so don't use this for real.
+                            // When printing grid, lower the number of chance cards or player will run out of time
+                            // system.printGrid(grid);
+                            // System.out.println("");
                             if(placingRoot){
                                 scoreGrid(row, col, grid); // Only when DEPTH_LIMIT == 1
                             }
@@ -199,14 +337,6 @@ public class SearchTreePlayer implements PokerSquaresPlayer {
         }
     }
 
-
-    private String printRemainingCards(Card[] deck){
-        String cardList = "";
-        for(int i=0; i<deck.length; i++){
-            cardList += deck[i] + ":";
-        }
-        return cardList;
-    }
 
     /* (non-Javadoc)
 	 * @see PokerSquaresPlayer#setPointSystem(PokerSquaresPointSystem, long)
